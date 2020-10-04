1b82625e318fad33b6f066cbcb0bff21ed438794
Viacheslav Tsarenkov <tsarenkovslava@yandex.ru>
2020-10-04 23:10:19 +0300

    feat
    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = n.toString().replace("-","");
+    let reversed = "";
+  for (let i = n.length-1; i >=0 ; --i){
+    reversed = reversed + n[i];
+  }
+  return reversed;
 }
